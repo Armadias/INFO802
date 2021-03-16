5299750a2e82075a81da4dd1fe1f55140f017ad6
Armadias <follietmartin@gmail.com>
2021-03-16 18:06:07 +0100

    changement pour faire fonctionner heroku

--- serveur.js
+++ serveur.js
@@ -5,44 +5,13 @@ var app = require('express')();
 var ejs = require('ejs');
 
 var api = require("request");
-
-var sSoap = 
-{
-    servicesCoutLivraison: 
-    {
-        prixLivraison: 
-        {
-            
-            calculCoutLivraison: function (args)
-            {
-                let prixParKg = 1;
-                let prixParKm = 2;
-                
-                let poids = args.poids || 0;
-                let distance = args.distance || 0;
-                
-                let prixPoids = prixParKg * poids;
-                let prixKm = prixParKm * distance;
-                
-                let prix = (prixPoids * 0.5) + (prixKm * 0.9);
-                
-                return {
-                    prixLivraison: prix
-                };
-            }
-        }
-    }
-};
-
-var xml = require("fs").readFileSync("serveur.wsdl", "utf8");
+const { Console } = require('console');
 
 let port = process.env.PORT || 3000;
 
 app.listen( port , function()
 {
-    soap.listen(app, '/wsdl', sSoap, xml, function () {
-        console.log(`Serveur lancé sur le port ${port}`);
-    });
+    console.log(`serveur lancé sur le port :${port}`)
 }
 );
 
